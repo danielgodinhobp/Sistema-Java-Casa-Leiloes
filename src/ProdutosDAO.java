43e36c223ff4cb6e1dff4df90cb0fd4f9751e9fd
Daniel Godinho <danielgodinho123@proton.me>
2024-10-28 13:17:07 -0300

    Implementada a navegação no botão Consultar Vendas para abrir a tela de Vendas

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -94,6 +94,39 @@ public class ProdutosDAO {
     } 
     }
     
+    
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+    conn = new conectaDAO().connectDB();
+    ArrayList<ProdutosDTO> produtosVendidos = new ArrayList<>();
+    String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+    
+    try {
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+            produtosVendidos.add(produto);
+        }
+    } catch (SQLException erro) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos vendidos: " + erro.getMessage());
+    } finally {
+        try {
+            if (resultset != null) resultset.close();
+            if (prep != null) prep.close();
+            if (conn != null) conn.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao fechar a conexão: " + e.getMessage());
+        }
+    }
+    return produtosVendidos;
+}
+
+    
 }
 
 
